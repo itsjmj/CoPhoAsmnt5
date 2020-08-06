0584c4310735561c2f402caca493a5ea16e77528
itsjmj <65914629+itsjmj@users.noreply.github.com>
2020-08-06 23:21:56 +0200

    final edit

--- Homework_5_function_headers.py
+++ Homework_5_function_headers.py
@@ -38,17 +38,26 @@ def pw_expansion(lam, m, n0, max_x, Nx):
             Matrix containing the analytical solution of the plane wave
             [matrix with dimensions Nx x Nx]            
     '''
+    #Defining initial parameters
     k = n0*2*np.pi/lam
+    
+    #Setting up arrays of coordinates
     x = np.linspace(-max_x,max_x,Nx)
     y = np.linspace(-max_x,max_x,Nx)
     X,Y = np.meshgrid(x,y)
     R = np.sqrt(X**2 + Y**2)
+    
+    #theta is calculated using np.arctan2 to ensure correct sign and angle within [-pi,pi]
     theta = np.arctan2(Y,X)
+    
+    #angular orders as vector
     m_vec = np.arange(-m,m+1,1)
     
+    #defining analytical field
     u_an = np.exp(1j*k*X)
     u = np.zeros((u_an.shape),dtype=complex)
     
+    #Summing terms over for loop
     for m in m_vec:
        u += (1j**m)*jv(m,k*R)*np.exp(-1j*m*theta)
     
@@ -91,7 +100,7 @@ def mie_pw(lam, m, n0, nc, Rad, pol, max_x, Nx):
             [matrix with dimensions Nx x Nx]
     '''
     
-    
+    #Defining initial parameters
     n1 = n0
     n2 = nc
     k1 = n1*2*np.pi/lam
@@ -103,22 +112,32 @@ def mie_pw(lam, m, n0, nc, Rad, pol, max_x, Nx):
     if pol == 'TM':
         p1 = n1**2
         p2 = n2**2
+        
+    #Setting up coordinate arrays
     x = np.linspace(-max_x,max_x,Nx)
     y = np.linspace(-max_x,max_x,Nx)
     X,Y = np.meshgrid(x,y)
     R = np.sqrt(X**2 + Y**2)
+    
+    #theta is calculated using np.arctan2 to ensure correct sign and angle within [-pi,pi]
     theta = np.arctan2(Y,X)
+    
+    #angular orders as vector
     m_vec = np.arange(-m,m+1,1)
     
+    #empty field array
     u = np.zeros((Nx,Nx),dtype=complex)
     
+    #performing sum over orders and calcuation am and bm coefficients at each step
     for m in m_vec:
        am = (jvp(m,z2)*jv(m,z1)*p1*n2 - jvp(m,z1)*jv(m,z2)*p2*n1)/(hvp(m,z1)*jv(m,z2)*p2*n1 - hv(m,z1)*jvp(m,z2)*p1*n2)
        bm = (hvp(m,z1)*jv(m,z1)*p2*n1 - hv(m,z1)*jvp(m,z1)*p2*n1)/(hvp(m,z1)*jv(m,z2)*p2*n1 - hv(m,z1)*jvp(m,z2)*p1*n2)
        
+       #only field in correct part of domain are summed
        u[R>Rad] += (1j**m)*am*hv(m,k1*R[R>Rad])*np.exp(-1j*m*theta[R>Rad])
        u[R<=Rad] += (1j**m)*bm*jv(m,k2*R[R<=Rad])*np.exp(-1j*m*theta[R<=Rad])
     
+    #analytical field used as incident field for better accuracy
     u[R>Rad] += np.exp(1j*k1*X[R>Rad])
     return u
 
@@ -161,7 +180,7 @@ def mie_gauss(lam, waist, m, n0, nc, Rad, pol, max_x, Nx):
             Matrix containing the amplitude of the Gaussian beam scattered at a cylinder
             [matrix with dimensions Nx x Nx]
     '''
- 
+    #Defining initial parameters 
     n1 = n0
     n2 = nc
     k1 = n1*2*np.pi/lam
@@ -173,32 +192,45 @@ def mie_gauss(lam, waist, m, n0, nc, Rad, pol, max_x, Nx):
     if pol == 'TM':
         p1 = n1**2
         p2 = n2**2
+        
+    #Setting up coordinate arrays
     x = np.linspace(-max_x,max_x,Nx)
     y = np.linspace(-max_x,max_x,Nx)
     X,Y = np.meshgrid(x,y)
     R = np.sqrt(X**2 + Y**2)
+    
+    #theta is calculated using np.arctan2 to ensure correct sign and angle within [-pi,pi]
     theta = np.arctan2(Y,X)
     m_vec = np.arange(-m,m+1,1)
     
+    #Empty initial field array
     u = np.zeros((Nx,Nx),dtype=complex)
     w = waist
+    
+    #Calculating frequency spacing and array of ky values to sum over
     rho = 2/w
-    dky = rho/10
+    dky = rho/5
     Nk = np.rint(k1/dky)*2 + 1
     Nk = int(Nk)
+    
+    #ky in [-k1,k1] to avoid imaginary kx
     ky_vec = np.linspace(-k1,k1,Nk) 
     dky = np.abs(ky_vec[1] - ky_vec[0])
+    
+    #Calculating double sum to solve gaussian scattering problem 
     for ky in ky_vec:
         print(ky)
         kx = np.sqrt(k1**2 - ky**2)
         thetak = np.arctan2(ky,kx)
         ck = (w/np.sqrt(4*np.pi))*np.exp(-(w*ky/2)**2)*dky
+        
+        #The incident field is calculated from cartesian plane waves to avoid error due to partial wave expansion
         u[R>Rad] += ck*np.exp(1j*(ky*Y[R>Rad] + kx*X[R>Rad]))
         for m in m_vec:
             am = (jvp(m,z2)*jv(m,z1)*p1*n2 - jvp(m,z1)*jv(m,z2)*p2*n1)/(hvp(m,z1)*jv(m,z2)*p2*n1 - hv(m,z1)*jvp(m,z2)*p1*n2)
             bm = (hvp(m,z1)*jv(m,z1)*p2*n1 - hv(m,z1)*jvp(m,z1)*p2*n1)/(hvp(m,z1)*jv(m,z2)*p2*n1 - hv(m,z1)*jvp(m,z2)*p1*n2)
             
             u[R>Rad] += ck*(1j**m)*am*hv(m,k1*R[R>Rad])*np.exp(-1j*m*(theta[R>Rad]-thetak)) 
-            u[R<=Rad] += ck*bm*(1j**m)*jv(m,k1*R[R<=Rad])*np.exp(-1j*m*(theta[R<=Rad]-thetak))
+            u[R<=Rad] += ck*bm*(1j**m)*jv(m,k2*R[R<=Rad])*np.exp(-1j*m*(theta[R<=Rad]-thetak))
 
     return u
--- Homework_5_testscript_gauss.py
+++ Homework_5_testscript_gauss.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import scipy.special
+import bluered_dark
 from Homework_5_function_headers import mie_pw, mie_gauss
 from matplotlib import pyplot as plt
 
@@ -22,18 +23,137 @@ plt.close('all')
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # geometry parameters
-max_x = ??  # width of computatinal domain [µm]
+max_x = 5  # width of computatinal domain [µm]
 waist = 3  # waist of the Gaussian beam [µm]
 n0 = 1     # refractive index of the surrounding
 nc = 1.5   # refractive index of the cylinder 
-Rad = ??    # radius of the cylinder [µm]
+Rad = 3    # radius of the cylinder [µm]
 
 # simulation parameters
 lam = 1   # wavelength [µm]
-Nx = ??  # number of grid points
-m = ??    # number of the expansion orders retained
+Nx = 200  # number of grid points
+m = 10    # number of the expansion orders retained
 
+x = np.linspace(-max_x,max_x,Nx)
+y = np.linspace(-max_x,max_x,Nx)
+X,Y = np.meshgrid(x,y)
 
 # %% run simulations and create representative figures of the results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# please add your code here
+#%%
+#Figures to test dky and order of gaussian incident beam
+pol = 'TE'
+u = mie_gauss(lam, waist, m, n0, nc, Rad, pol, max_x, Nx)
+
+#%%
+pol = 'TE'
+if pol == 'TE':
+    u_field = r'$E_z$'
+elif pol == 'TM':
+    u_field = r'$E_z$'
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.ticker as tk
+def colorbar(mappable):
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.07)
+    fmt = tk.ScalarFormatter(useOffset=True)
+    fmt.set_scientific(1)
+    return fig.colorbar(mappable, format=fmt,cax=cax)
+
+#%%
+plt.rcParams.update({'font.size': 10})
+f1, ax = plt.subplots(dpi=130)
+ax.axis('equal')
+cm = ax.pcolormesh(X,Y,u.real,cmap = 'rainbow')
+cb = colorbar(cm)
+ax.set_xlabel(r'x [$\mu$m]')
+ax.set_ylabel(r'y [$\mu$m]')
+ax.set_title(r'm = 10, $\Delta k_y = \rho/2$')
+cb.set_label('Re['+u_field+']'+' [A.u.]')
+f1.tight_layout()
+f1.savefig('ptest_m10_r2.png',bbox_inches='tight')
+plt.show()
+#%%
+rdiv_range = np.rint(np.linspace(3,90,3))
+print(rdiv_range)
+#%%
+#Figures showing Gaussian scattering over radius range
+#rdiv_range = np.rint(np.linspace(3,95,4))
+rdiv_range = np.array([2.5,50,99])
+u_TE = []
+u_TM = []
+for rdiv in rdiv_range:
+    ute = mie_gauss(lam, waist, m, n0, nc, lam/rdiv, 'TE', max_x, Nx)
+    utm = mie_gauss(lam, waist, m, n0, nc, lam/rdiv, 'TM', max_x, Nx)
+    u_TE.append(ute)
+    u_TM.append(utm)
+    
+#%%
+
+plt.rcParams.update({'font.size': 8})
+f2, axs = plt.subplots(3,2,dpi=130)
+axsf = axs.flatten()
+for ax in axsf:
+    ax.axis('equal')
+
+for i in range(len(u_TE)):
+    uTE = u_TE[i]
+    uTM = u_TM[i]
+    cm1 = axs[i,0].pcolormesh(X,Y,uTE.real,cmap = 'rainbow')
+    axs[i,0].set_xlabel(r'x [$\mu$m]')
+    axs[i,0].set_ylabel(r'y [$\mu$m]')
+    axs[i,0].set_title(r'Rad = $\lambda$'+'/{}, TE'.format(rdiv_range[i]))
+    axs[i,0].add_artist(plt.Circle((0, 0), lam/rdiv_range[i], facecolor='none', edgecolor='k',lw=0.3))
+    cb1 = colorbar(cm1)
+    cb1.set_label(r'Re[$E_z$]'+' [A.u.]')
+    
+    cm2 = axs[i,1].pcolormesh(X,Y,uTM.real,cmap = 'rainbow')
+    axs[i,1].set_xlabel(r'x [$\mu$m]')
+    axs[i,1].set_ylabel(r'y [$\mu$m]')
+    axs[i,1].set_title(r'Rad = $\lambda$'+'/{}, TM'.format(rdiv_range[i]))
+    axs[i,1].add_artist(plt.Circle((0, 0), lam/rdiv_range[i], facecolor='none', edgecolor='k',lw=0.3))
+    cb2 = colorbar(cm2)
+    cb2.set_label(r'Re[$H_z$]'+' [A.u.]')
+    
+    
+    
+f2.tight_layout()
+#f2.savefig('re_gauss_vary_rad.png',bbox_inches='tight')
+plt.show()
+
+#%%
+f3, axs = plt.subplots(3,2,dpi=130)
+axsf = axs.flatten()
+for ax in axsf:
+    ax.axis('equal')
+
+for i in range(len(u_TE)):
+    uTE = u_TE[i]
+    uTM = u_TM[i]
+    cm1 = axs[i,0].pcolormesh(X,Y,np.abs(uTE)**2,cmap = 'magma')
+    axs[i,0].set_xlabel(r'x [$\mu$m]')
+    axs[i,0].set_ylabel(r'y [$\mu$m]')
+    axs[i,0].set_title(r'Rad = $\lambda$'+'/{}, TE'.format(rdiv_range[i]))
+    axs[i,0].add_artist(plt.Circle((0, 0), lam/rdiv_range[i], facecolor='none', edgecolor='k',lw=0.3))
+    cb1 = colorbar(cm1)
+    cb1.set_label(r'$|E_z|^2$'+' [A.u.]')
+    
+    cm2 = axs[i,1].pcolormesh(X,Y,np.abs(uTM)**2,cmap = 'magma')
+    axs[i,1].set_xlabel(r'x [$\mu$m]')
+    axs[i,1].set_ylabel(r'y [$\mu$m]')
+    axs[i,1].set_title(r'Rad = $\lambda$'+'/{}, TM'.format(rdiv_range[i]))
+    axs[i,1].add_artist(plt.Circle((0, 0), lam/rdiv_range[i], facecolor='none', edgecolor='k',lw=0.3))
+    cb2 = colorbar(cm2)
+    cb2.set_label(r'$|H_z|^2$'+' [A.u.]')
+    
+    
+    
+f3.tight_layout()
+#f3.savefig('abs_gauss_vary_radpng',bbox_inches='tight')
+plt.show()
+    
+
